bc68c63772c831ca591d4fd310136170f51e936a
stiven <stivn.4@gmail.com>
2026-05-19 12:03:16 -0500

    cadena nueva

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Hello, platzi!");
 
 string stripe_api_key="sk_test_4eC39HqLyjWDarjt1zdp7dc";
+
+string testingConnection = "Server=tcp:aminnesserver.database.windows.net,1433;Initial Catalog=aminnesdb;Persist Security Info=False;User ID=adminaminnes;Password=********;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
